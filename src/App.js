848969774b0169937784b59a58c839b7c0bb2544
hyesuelee <hyesue12@gmail.com>
2018-06-22 23:10:27 +0900

    Dataflow with Props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movies = [ //영화 List 있다고 가정..  
+  "Before Sunrise",
+  "Head on",
+  "Midnight in Paris",
+  "Shape of water",
+  "Avengers"
+]
+
+/*이영화들의 제목을 자식 컴포넌트인 movie컴포넌트로 보내고싶다면
+부모 comp: <Movie title={movies[0]}/> : 자식에게 제목보냄.
+자식 comp: console.log(this.props); 자식컴포넌트는 부모에게 정보 얻기위해 props사용 */
+
+
 class App extends Component { // Component called App  
   render() {//출력기능 
       return( 
       <div className="App">
-       <Movie /> 
+       <Movie title={movies[0]}/> 
+       <Movie title={movies[1]}/> 
+       <Movie title={movies[2]}/> 
+       <Movie title={movies[3]}/> 
+       <Movie title={movies[4]}/> 
       </div>
       );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,9 +3,25 @@ import './Movie.css';
 
 class Movie extends Component{ //클래스 생성(컴포넌트) 
     render(){ //컴포넌트는 무조건 render!
-        return(// 랜더를 리턴해주어야한다!! 꼭! 
-        <h1> Hello, this is a movie.</h1>
+        /*부모 app comp로부터 title정보 얻기위해서
+        props 사용!! */
+        console.log(this.props);
+        // 브라우저 요소검사시 콘솔에서 부모comp의 정보 들을 확인할 수 있다. 
+        return(// 랜더를 리턴해주어야한다!! 꼭!
+        <div>
+            <MoviePoster />
+            <h1>{this.props.title}</h1>
+            <h1>{this.props.title}</h1>
+        </div>
         )       
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <img src="https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg"/>
+        )
     }
 }
 
