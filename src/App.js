95f8d92f62b0f9fef8cc137f34074b8380036c5e
hyesuelee <hyesue12@gmail.com>
2018-06-29 23:17:14 +0900

    dataflow with props2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [ //영화 List 있다고 가정..  
+const movieTitles = [ //영화 List 있다고 가정..  
   "Before Sunrise",
   "Head on",
   "Midnight in Paris",
   "Shape of water",
   "Avengers"
+]
+
+const moviePosters = [
+  "https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg",
+  "https://media2.fdncms.com/chicago/imager/head-on/u/zoom/5723772/head_on.jpg", 
+  "https://images-na.ssl-images-amazon.com/images/I/710PQRUAl-L._SY445_.jpg",
+  "https://cdn-images-1.medium.com/max/1382/1*NZhRKM4xvz27VLl5vJ73Sg.jpeg",
+  "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg" 
 ]
 
 /*이영화들의 제목을 자식 컴포넌트인 movie컴포넌트로 보내고싶다면
@@ -19,12 +27,13 @@ class App extends Component { // Component called App
   render() {//출력기능 
       return( 
       <div className="App">
-       <Movie title={movies[0]}/> 
-       <Movie title={movies[1]}/> 
-       <Movie title={movies[2]}/> 
-       <Movie title={movies[3]}/> 
-       <Movie title={movies[4]}/> 
+       <Movie poster={moviePosters[0] } title={movieTitles[0]}/>
+       <Movie poster={moviePosters[1] } title={movieTitles[1]}/>
+       <Movie poster={moviePosters[2] } title={movieTitles[2]}/>
+       <Movie poster={moviePosters[3] } title={movieTitles[3]}/>
+       <Movie poster={moviePosters[4] } title={movieTitles[4]}/>
       </div>
+    
       );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -7,11 +7,14 @@ class Movie extends Component{ //클래스 생성(컴포넌트)
         props 사용!! */
         console.log(this.props);
         // 브라우저 요소검사시 콘솔에서 부모comp의 정보 들을 확인할 수 있다. 
-        return(// 랜더를 리턴해주어야한다!! 꼭!
+        return(// 랜더를 리턴해주어야한다!! 꼭! 
+        //요소 엑세스: {this.props.[props's name]} //{}: jsx명령 괄호.
         <div>
-            <MoviePoster />
-            <h1>{this.props.title}</h1>
-            <h1>{this.props.title}</h1>
+            <MoviePoster poster={this.props.poster}/>
+            <h1>{this.props.title}</h1> 
+
+             
+           
         </div>
         )       
     }
@@ -20,9 +23,10 @@ class Movie extends Component{ //클래스 생성(컴포넌트)
 class MoviePoster extends Component{
     render(){
         return(
-            <img src="https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg"/>
+            <img src={this.props.poster}/>
         )
     }
+    
 }
 
 export default Movie // app.js로 해당 컴포넌트 export! 
